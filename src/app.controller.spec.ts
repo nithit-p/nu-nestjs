a2d0fe720e4af4db60a416d88aebef6e57ffeb3a
Nithit.p <nithit.prayongsap@gmail.com>
2023-07-04 23:53:36 +0700

    update testcase

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello World!!🤣🤣🤣🤣"', () => {
+      expect(appController.getHello()).toBe('Hello World!!🤣🤣🤣🤣');
     });
   });
 });
